1c4d5d5f863a5627b853ebaf7f694004c1a8625a
Rafael Polutta <rafael@diccce.com>
2017-06-09 08:51:48 +0200

    Mobile ready
    Added proper mobile support.

--- site/modules/preview-left/preview-left.html.php
+++ site/modules/preview-left/preview-left.html.php
@@ -1,26 +1,26 @@
 <div class="preview-module wow fadeInUp">
 
-    <div class="group preview-text">
-        <div class="col col-4"></div>
-        <div class="col col-3">
-          <h2><?= kirbytext($module->headline()) ?></h2>
-          <?= kirbytext($module->text()) ?>
-          <a href="<?= html($module->link()) ?>" target="_blank" class="link"><?= html($module->linktitle()) ?></a>
-        </div>
-        <div class="col col-1"></div>
-    </div>
+  <div class="group preview-image" data-bottom-top="transform:translateY(40px);" data-top-bottom="transform:translateY(-40px);">
+    <div class="col col-5">
+      <?php
+      // try to get an image object      
+      $image = $module->previewImage()->toFile();     
+      // check if image exists     
+      if($image): ?>     
+      <img src="<?php echo $image->url(); ?>" alt="">      
+      <?php endif ?>    
+    </div>    
+    <div class="col col-3"></div>
+  </div>
 
-    <div class="group preview-image" data-bottom-top="transform:translateY(40px);" data-top-bottom="transform:translateY(-40px);">
-    	<div class="col col-5">
-    		<?php
-		    // try to get an image object
-		    $image = $module->previewImage()->toFile();
-		    // check if image exists
-		    if($image): ?>
-		    <img src="<?php echo $image->url(); ?>" alt="">
-		    <?php endif ?>
-		  </div>
-		  <div class="col col-3"></div>
-	</div>
+  <div class="group preview-text">
+      <div class="col col-4"></div>
+      <div class="col col-3">
+        <h2><?= kirbytext($module->headline()) ?></h2>
+        <?= kirbytext($module->text()) ?>
+        <a href="<?= html($module->link()) ?>" target="_blank" class="link"><?= html($module->linktitle()) ?></a>
+      </div>
+      <div class="col col-1"></div>
+  </div>
 
 </div>
